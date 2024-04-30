2617a6a8445984d0d59402652c101effb8d5ee41
kanatatsu64 <kanatatsu64@gmail.com>
2024-04-30 14:50:51 +0900

    basic ssr

--- .esbuild-node.mjs
+++ .esbuild-node.mjs
@@ -1,11 +1,12 @@
 import * as esbuild from "esbuild";
 
 await esbuild.build({
-  entryPoints: ["app/server.ts"],
+  entryPoints: ["app/server.tsx"],
   bundle: true,
   outfile: "public/server.js",
   loader: {
     ".ts": "ts",
+    ".tsx": "tsx",
   },
   format: "esm",
   target: "es2015",
